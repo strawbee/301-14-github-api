fb77702c5238a19bd6a64102480a4fff05b2ba84
strawbee <yuejiao.hou@gmail.com>
2017-11-10 13:05:11 -0800

    complete

--- public/app.js
+++ public/app.js
@@ -1,13 +1,21 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
+// token, port, npm install
 
-$.ajax({
-  url: 'https://api.github.com/user/repos?type=owner',
-  method: 'GET',
-  headers: {
-    Authorization: 'token copy-pasta-your-token-here'
-  }
-})
+// $.ajax({
+//   url: 'https://api.github.com/user/repos?type=owner',
+//   method: 'GET',
+//   headers: {
+//     Authorization: 'token copy-pasta-your-token-here'
+//   }
+// })
+//   .then(
+//     data => data.forEach(repo =>
+//       $('#results').append(`<h3>${repo.name}</h3><p>${repo.description}</p><hr>`)),
+//     err => console.error(err.status, err.statusText, 'is the way my stuff is broken'));
+
+
+$.get('/github/user/repos')
   .then(
-    data => data.forEach(repo =>
+    data => JSON.parse(data).forEach(repo =>
       $('#results').append(`<h3>${repo.name}</h3><p>${repo.description}</p><hr>`)),
     err => console.error(err.status, err.statusText, 'is the way my stuff is broken'));
--- server.js
+++ server.js
@@ -3,11 +3,27 @@ const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
 
+// handles proxy requests
+const superagent = require('superagent');
+
 app.use(express.static('./public'));
 app.use(cors());
 
+// Proxy route for handling GitHub API requests
+app.get('/github/*', (req, res) => {
+  console.log(req.params[0]);
+  const url = `https://api.github.com/${req.params[0]}`;
+  superagent(url)
+    .set(`Authorization`, `token ${process.env.GITHUB_TOKEN}`)
+    // .then (success callback, fail callback)
+    .then (
+      repos => res.send(repos.text),
+      err => res.send(err)
+    )
+});
+
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  res.sendFile('index.html');
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
